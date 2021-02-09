02ce71b071ae0b77236ee126fe3b469f1d3f8339
kyleorz213 <2395931623@qq.com>
2021-02-09 20:18:23 +0800

    🚀 finished and deployed

---
+++ src/Counter.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./Counter.css";
+
+function Counter() {
+  const [count, setCount] = useState(10);
+  const [countBefore, setCountBefore] = useState(9);
+  const [countAfter, setCountAfter] = useState(11);
+  const [isBefore, setIsBefore] = useState(false);
+  const [isAfter, setIsAfter] = useState(false);
+
+  const subtract = () => {
+    setIsBefore(true);
+    console.log(isBefore);
+    setTimeout(() => {
+      setCount(count - 1);
+      setCountBefore(countBefore - 1);
+      setCountAfter(countAfter - 1);
+      setIsBefore(false);
+    }, 200);
+  };
+  const add = () => {
+    setIsAfter(true);
+    setTimeout(() => {
+      setCount(count + 1);
+      setCountBefore(countBefore + 1);
+      setCountAfter(countAfter + 1);
+      setIsAfter(false);
+    }, 200);
+  };
+
+  return (
+    <div id="counter">
+      <button onClick={subtract} disabled={isBefore || isAfter}>
+        <i className="fas fa-minus"></i>
+      </button>
+      <span
+        data-before={countBefore}
+        data-after={countAfter}
+        className={`${isBefore ? "before" : ""}${isAfter ? "after" : ""}`}
+      >
+        {count}
+      </span>
+      <button onClick={add} disabled={isBefore || isAfter}>
+        <i className="fas fa-plus"></i>
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Counter from "./Counter";
 import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode></React.StrictMode>,
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>,
   document.getElementById("root")
 );
